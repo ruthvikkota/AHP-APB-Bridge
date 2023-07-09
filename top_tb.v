9dcf8f4b4bcdfc05b8eeb0ec994b402cebb1ead5
Ruthvik Kota <88280622+ruthvikkota@users.noreply.github.com>
2023-07-09 23:15:01 +0530

    Update top_tb.v

--- top_tb.v
+++ top_tb.v
@@ -1,45 +1,18 @@
-module top_tb();
-reg hclk;
-reg hresetn;
-
-wire [31:0] hr_data, haddr, hwdata, pr_data, paddr,pwdata,paddr_out,pwdata_out;
-wire hwrite,hready_in;
-wire [1:0] htrans;
-wire [1:0] hres =0;
-wire penable,pwrite,hr_readyout, pwrite_out,penable_out;
-wire [2:0] psel,psel_out;
-
-ahb_master AHB (hclk,hresetn,hr_readyout, hr_data,haddr,hwdata, hwrite,hready_in,htrans);
-
-bridge_top BRIDGE(hclk,hresetn,hwrite,hready_in,htrans,hwdata,haddr,pr_data,penable,pwrite,hr_readyout,psel, hres,paddr,hr_data);
-
-apb_interface APB ( pwrite,penable, psel, paddr,pwdata, pwrite_out,penable_out,  psel_out,  paddr_out,pwdata_out, pr_data);
-
-
-initial
-begin
-hclk=1'b0;
-forever #10 hclk=~hclk;
-end
-
-task reset;
-begin
-@(negedge hclk);
-hresetn= 1'b0;
-@(negedge hclk);
-hresetn= 1'b1;
-end
-endtask
-
-initial
-begin
-reset;
-//ahb.single_write();
+module top_tb(); reg hclk,hresetn;
+wire hready_out,hwrite,hready_in;
+wire [31:0]hr_data,haddr,hwdata,paddr,pwdata,paddrout,pwdataout,pr_data; wire [1:0]htrans;
+wire [1:0]hres=0;
+wire penable,pwrite,pwriteout,penableout; wire [2:0]psel,pselout;
+ahb_master ahb(hclk,hresetn,hready_out,hr_data,haddr,hwdata,hwrite,hready_in,htrans);
+bridge_top bridge (hclk,hresetn,hwrite, hready_in,htrans,hwdata,haddr,pr_data,penable,pwrite,hready_out,psel,hres,paddr ,pwdata , hr_data );
+apb_interface apb(pwrite,psel,penable,paddr,pwdata,pwriteout,pselout,penableout,paddrout,pwdataout,pr_data); initial
+begin hclk=1'b0;
+forever #10 hclk=~hclk; end
+task reset(); begin @(negedge hclk) hresetn=1'b0; @(negedge hclk) hresetn=1'b1; end
+endtask initial begin reset;
+ahb.single_write();
 //ahb.single_read();
 //ahb.burst_4_incr_write();
-ahb.burst_incr4_read();
-
+//ahb.burst_incr4_read();
 end
 endmodule
-
-
