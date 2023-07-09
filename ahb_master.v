d782bf9377d4b0dc0918f18cd2e2c213f9f1d952
Ruthvik Kota <88280622+ruthvikkota@users.noreply.github.com>
2023-07-09 23:14:07 +0530

    Update ahb_master.v

--- ahb_master.v
+++ ahb_master.v
@@ -1,56 +1,57 @@
-module ahb_master (input hclk,hresetn,hreadyout, input [31:0] hrdata,
-output reg [31:0]haddr,hwdata,output reg hwrite,hready_in,htrans);
-
-reg [2:0] hburst;  // 4,16,...
-reg [2:0] hsize;  //8bit,16bit,...
-integer i;
-
-task single_write();
+module ahb_master (
+input hclk,hresetn, hreadyout, input [31:0] hrdata,
+output reg [31:0] haddr,hwdata, output reg hwrite,hready_in, output reg [1:0] htrans);
+reg [2:0] hburst;
+reg [2:0] hsize; integer i;
+task single_write(); begin
+@(posedge hclk) #1;
+begin hwrite = 1;
+htrans = 2'd2; hsize = 0;
+hburst = 0;
+hready_in = 1;
+haddr = 32'h8000_0000; end
+@(posedge hclk) #1;
 begin
-@(posedge hclk)
-#1;
-
+hwdata = 32'h24; htrans = 2'd0; end
+end endtask
+task single_read(); begin
+@(posedge hclk) #1;
+begin hwrite = 0;
+htrans = 2'd2; hsize = 0;
+hburst = 0;
+hready_in = 1;
+haddr = 32'h8000_0000; end
+@(posedge hclk) #1;
 begin
-hwrite=1;
-htrans=2'd2;
-hsize=0;
-hburst=0;
-hready_in=1;
-haddr= 32'h8000_0000;
-end
-
-@(posedge hclk)
-#1;
+htrans = 2'd0; end
+end endtask
+task burst_4_incr_write(); begin
+@(posedge hclk); #1
+begin hwrite = 1;
+htrans = 2'd2; hsize = 0;
+hburst = 3'b001; hready_in = 1;
+haddr = 32'h8000_0000; end
+@(posedge hclk); #1
+begin hwdata={$random}%256; haddr = haddr + 1;
+htrans = 2'd3; end
+for(i=1;i<3;i=i+1) begin @(posedge hclk);
+#1
 begin
-hwdata=32'h24;
-htrans=2'd0;
+htrans = 2'd3; hwdata={$random}%256; haddr = haddr + 1;
+end @(posedge hclk); end
+@(posedge hclk); #1
+begin hwdata={$random}%256; htrans = 2'd0;
+end end endtask
+task burst_incr4_read(); begin
+@(posedge hclk) #1;
+begin hwrite=0; htrans=2'd2; hsize=0; hburst=3'b001; hready_in=1;
+haddr=32'h8000_0000; end
+for(i=0;i<3;i=i+1) begin
+@(posedge hclk); #1;
+begin haddr=haddr+1; htrans=2'd3; end
+@(posedge hclk);
 end
-
-end
-endtask
-
-task sinle_read();
-begin
-@(posedge hclk)
-#1;
-
-begin
-hwrite=0;
-htrans=2'd2;
-hsize=0;
-hburst=0;
-hready_in=1;
-haddr= 32'h8000_0000;
-end
-
-@(posedge hclk)
-#1;
-begin
-htrans=2'd0;
-end
-
-end
-endtask
-
+@(posedge hclk) #1;
+begin htrans=2'd0; end
+end endtask
 endmodule
-
