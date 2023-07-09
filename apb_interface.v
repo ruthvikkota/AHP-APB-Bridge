55b660bc84eda41287dc4145972cecea871107ad
Ruthvik Kota <88280622+ruthvikkota@users.noreply.github.com>
2023-07-09 23:13:06 +0530

    Update apb_interface.v

--- apb_interface.v
+++ apb_interface.v
@@ -1,21 +1,10 @@
-module apb_interface(input pwrite,penable, input [2:0] psel,input [31:0] paddr,pwdata,
-output pwrite_out,penable_out, output [2:0] psel_out, output [31:0] paddr_out,pwdata_out,
-output reg [31:0] pr_data);
-
-assign pwirte_out = pwrite;
-assign paddr_out = paddr;
-assign psel_out= psel;
-assign pwdata_out = pwdata;
-assign penable_out = penable;
-
-always@ (*)
-begin
-if (!pwrite && penable)
-pr_data= {$random}&256;
-else
-pr_data= 32'h0;
-end
-
-endmodule
-
-
+module apb_interface(pwrite,psel,penable,paddr,pwdata,pwriteout,pselout,penableout,paddrout,pwdataout,pr_data); 
+input pwrite,penable;
+input [2:0]psel;
+input [31:0]pwdata,paddr; output pwriteout,penableout; output [2:0]pselout;
+output [31:0]pwdataout,paddrout; output reg[31:0]pr_data;
+assign penableout=penable; assign pselout=psel;
+assign pwriteout=pwrite; assign paddrout=paddr; assign pwdataout=pwdata;
+always @(*) begin
+if(~pwrite && penable) pr_data=($random)%256; else
+pr_data=0; end endmodule
